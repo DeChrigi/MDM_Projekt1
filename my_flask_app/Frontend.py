1e38f04f89f587d51b591fc3445481e6dd694484
DeChrigi <schweizerchristian50@gmail.com>
2024-03-31 14:40:58 +0200

    Changes to be committed: 	new file:   .vscode/launch.json 	modified:   LinearRegressor.pkl 	modified:   my_flask_app/Frontend.py

--- my_flask_app/Frontend.py
+++ my_flask_app/Frontend.py
@@ -1,18 +1,64 @@
 from flask import Flask, render_template, request
-import plotly.graph_objects as go
-import plotly.io as pio
-import DBHandler as dbh
 import ChartHandler as ch
-import pandas as pd
-import ModelHandler
+import os
+from dotenv import load_dotenv
+from azure.storage.blob import BlobServiceClient
+import pickle
+from pathlib import Path
+import numpy as np
+
+
+load_dotenv()
 
 app = Flask(__name__)
 
+# init app, load model from storage
+print("*** Init and load model ***")
+if os.getenv("AZURE_STORAGE_CONNECTION_STRING") != None:
+    azureStorageConnectionString = os.environ['AZURE_STORAGE_CONNECTION_STRING']
+    blob_service_client = BlobServiceClient.from_connection_string(azureStorageConnectionString)
+
+    print("fetching blob containers...")
+    containers = blob_service_client.list_containers(include_metadata=True)
+    for container in containers:
+        existingContainerName = container['name']
+        print("checking container " + existingContainerName)
+        if existingContainerName.startswith("youtube-model"):
+            parts = existingContainerName.split("-")
+            print(parts)
+            suffix = 1
+            if (len(parts) == 3):
+                newSuffix = int(parts[-1])
+                if (newSuffix > suffix):
+                    suffix = newSuffix
+
+    container_client = blob_service_client.get_container_client("youtube-model-" + str(suffix))
+    blob_list = container_client.list_blobs()
+    for blob in blob_list:
+        print("\t" + blob.name)
+
+    # Download the blob to a local file
+    Path("../model").mkdir(parents=True, exist_ok=True)
+    download_file_path = os.path.join("../model", "LinearRegressor.pkl")
+    print("\nDownloading blob to \n\t" + download_file_path)
+
+    with open(file=download_file_path, mode="wb") as download_file:
+         download_file.write(container_client.download_blob(blob.name).readall())
+
+else:
+    print("CANNOT ACCESS AZURE BLOB STORAGE - Please set connection string as env variable")
+    print(os.getenv("AZURE_STORAGE_CONNECTION_STRING"))
+    print("AZURE_STORAGE_CONNECTION_STRING not set")   
+
+file_path = Path(".", "./", "LinearRegressor.pkl")
+with open(file_path, 'rb') as fid:
+    model = pickle.load(fid)
+
 def predict_likes(views, comment_count, duration):
-    # Prepare the input features as a numpy array
+    print('OUTPUTOUTPUTOUTPUT2: ', views, comment_count, duration)
     
     # Make prediction using the provided model
-    prediction = ModelHandler.predict(views, comment_count, duration)
+    prediction = model.predict(np.array([[views, comment_count, duration]]))
     
     return prediction[0]  # Extract the predicted value from the array
 
