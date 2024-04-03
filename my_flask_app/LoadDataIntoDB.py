bdbde8c27461c4adec19002af16167d07f07335e
DeChrigi <schweizerchristian50@gmail.com>
2024-04-03 18:40:41 +0200

    deleted:    my_flask_app/LoadDataIntoDB.py

--- my_flask_app/LoadDataIntoDB.py
+++
@@ -1,5 +0,0 @@
-import DBHandler as dbh
-
-dbh.saveMostPopularVideosByCountryGeneric('CH', 50)
-dbh.saveMostPopularVideosByCountryGeneric('DE', 50)
-dbh.saveMostPopularVideosByCountryGeneric('FR', 50)
